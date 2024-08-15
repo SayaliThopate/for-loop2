6d8865cfe0205f00b0bc927131c92a06c020cb8c
SayaliThopate <158029728+SayaliThopate@users.noreply.github.com>
2024-08-16 00:32:48 +0530

    Add files via upload

---
+++ 4.dart
@@ -0,0 +1,7 @@
+void main(){
+  int squ=0;
+  for(int i=100;i<=120;i++){
+    squ=i*i;
+    print(squ);
+  }
+}
---
+++ 9.dart
@@ -0,0 +1,10 @@
+void main(){
+  int squ=0;
+  int sum=0;
+  for (int i=1;i<=15;i++){
+    squ=i*i;
+    print(squ);
+    sum=sum+squ;
+  }
+  print(sum);
+}
